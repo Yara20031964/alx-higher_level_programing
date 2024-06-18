2493ee83463de787a6ad40eb29c649fadee19201
yara20031964 <yara200359@gmail.com>
2024-06-19 02:21:26 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /* this is check the number of argv */
 if (process.argv.length === 2) {
-  console.log('No argument');
+  	console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Argument found');
+  	console.log('Argument found');
 } else {
-  console.log('Argument found');
+  	console.log('Argument found');
 }
 
